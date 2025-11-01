24e997bb7fdf5c04c93796fce097ceaf7524d61b
Makon324 <makon324@gmail.com>
2025-11-01 23:23:49 +0100

    Emulator: add device that just prints to console

--- Emulator/Emulator/IOPorts.cs
+++ Emulator/Emulator/IOPorts.cs
@@ -103,7 +103,21 @@ namespace Emulator
         }
     }
 
+    /// <summary>
+    /// A simple output device that prints the stored byte as a character to the command line.
+    /// </summary>
+    internal sealed class ConsoleOutputDevice : IOPort
+    {
+        public void PortStore(byte value)
+        {
+            Console.Write((char)value);
+        }
 
+        public byte PortLoad()
+        {           
+            return 0; // Loading from this device is not supported; return 0 as a default.
+        }
+    }
 
 
 

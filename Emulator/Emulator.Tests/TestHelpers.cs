f4f72ebc0d5fc67216bac8f198e0904c528af5f1
Makon324 <makon324@gmail.com>
2025-11-05 00:10:11 +0100

    Emulator: Add GetRandomRegister and GetTwoRandomDistinctRegisters to test Helpers

--- Emulator/Emulator.Tests/TestHelpers.cs
+++ Emulator/Emulator.Tests/TestHelpers.cs
@@ -11,5 +11,31 @@
         {
             return new CPUContext();
         }
+
+        /// <summary>
+        /// Returns a random register except R0.
+        /// </summary>
+        public static Register GetRandomRegister()
+        {
+            return (Register)Random.Next(1, Architecture.REGISTER_COUNT);
+        }
+
+        /// <summary>
+        /// Returns two distinct random registers except R0 as a tuple.
+        /// </summary>
+        public static (Register regA, Register regB) GetTwoRandomDistinctRegisters()
+        {
+            int indexA = Random.Next(1, Architecture.REGISTER_COUNT);
+            int indexB;
+
+            do
+            {
+                indexB = Random.Next(1, Architecture.REGISTER_COUNT);
+            } while (indexB == indexA);
+
+            return ((Register)indexA, (Register)indexB);
+        }
+
+        private static readonly Random Random = new Random();
     }
 }

bc0d5fcdd3d3fd58cc25721571c79cddfb88e7d2
Makon324 <makon324@gmail.com>
2025-11-08 20:58:16 +0100

    Emulator: move ConsoleOutputDevice into new file: IODevices

---
+++ Emulator/Emulator/IODevices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulator
+{
+    /// <summary>
+    /// A simple output device that prints the stored byte as a character to the command line.
+    /// </summary>
+    internal sealed class ConsoleOutputDevice : IOPort
+    {
+        public void PortStore(byte value)
+        {
+            Console.Write((char)value);
+        }
+
+        public byte PortLoad()
+        {
+            return 0; // Loading from this device is not supported; return 0 as a default.
+        }
+    }
+}
--- Emulator/Emulator/IOPorts.cs
+++ Emulator/Emulator/IOPorts.cs
@@ -96,23 +96,7 @@
         {
             return Value;
         }
-    }
-
-    /// <summary>
-    /// A simple output device that prints the stored byte as a character to the command line.
-    /// </summary>
-    internal sealed class ConsoleOutputDevice : IOPort
-    {
-        public void PortStore(byte value)
-        {
-            Console.Write((char)value);
-        }
-
-        public byte PortLoad()
-        {           
-            return 0; // Loading from this device is not supported; return 0 as a default.
-        }
-    }
+    }    
 
 
 

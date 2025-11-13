27394111865a03349ef15af8c04e0ea135a883d0
Makon324 <makon324@gmail.com>
2025-11-13 21:08:44 +0100

    Emulator: Move ProjectPathResolver from ProgramLoading.cs into Global.cs

--- Emulator/Emulator/Global.cs
+++ Emulator/Emulator/Global.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Emulator
 {
@@ -18,5 +14,46 @@ namespace Emulator
                 throw new InvalidOperationException($"Service {typeof(T)} not registered.");
             return Services.GetRequiredService<T>();
         }
+    }
+
+    internal static class ProjectPathResolver
+    {
+        /// <summary>
+        /// Finds the solution root directory by looking for .git folder
+        /// </summary>
+        /// <returns>The full path to the solution root directory</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown when solution root cannot be found</exception>
+        public static string FindSolutionRoot()
+        {
+            if (_cache != null) return _cache;
+
+            // Start from the current assembly's location
+            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            var startDirectory = Path.GetDirectoryName(assemblyLocation);
+
+            if (string.IsNullOrEmpty(startDirectory))
+            {
+                throw new DirectoryNotFoundException("Could not determine assembly directory");
+            }
+
+            var directory = new DirectoryInfo(startDirectory);
+
+            // Traverse up until we find .git
+            while (directory != null)
+            {
+                if (directory.GetDirectories(".git").Any())
+                {
+                    _cache = directory.FullName;
+                    return directory.FullName;
+                }
+
+                directory = directory.Parent;
+            }
+
+            throw new DirectoryNotFoundException(
+                "Solution root not found looking for .git folder.");
+        }
+
+        private static string? _cache = null;
     }
 }
--- Emulator/Emulator/ProgramLoading.cs
+++ Emulator/Emulator/ProgramLoading.cs
@@ -55,47 +55,6 @@ namespace Emulator
         }
     }
 
-    internal static class ProjectPathResolver
-    {
-        /// <summary>
-        /// Finds the solution root directory by looking for .git folder
-        /// </summary>
-        /// <returns>The full path to the solution root directory</returns>
-        /// <exception cref="DirectoryNotFoundException">Thrown when solution root cannot be found</exception>
-        public static string FindSolutionRoot()
-        {
-            if (_cache != null) return _cache;
-
-            // Start from the current assembly's location
-            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
-            var startDirectory = Path.GetDirectoryName(assemblyLocation);
-
-            if (string.IsNullOrEmpty(startDirectory))
-            {
-                throw new DirectoryNotFoundException("Could not determine assembly directory");
-            }
-
-            var directory = new DirectoryInfo(startDirectory);
-
-            // Traverse up until we find .git
-            while (directory != null)
-            {
-                if (directory.GetDirectories(".git").Any())
-                {
-                    _cache = directory.FullName;
-                    return directory.FullName;
-                }
-
-                directory = directory.Parent;
-            }
-
-            throw new DirectoryNotFoundException(
-                "Solution root not found looking for .git folder.");
-        }
-
-        private static string? _cache = null;
-    }
-
     internal readonly record struct Token(string Type, string Value, int Line, int StartColumn);
     internal enum StatementType
     {
